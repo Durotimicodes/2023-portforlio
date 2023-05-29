26a2c49a02bdb6f7e74b88aee5a6521d319d9627
edmond fagbuyi <edmondfagbuyi@gmail.com>
2023-05-29 02:29:50 +0100

    adding 3d balls

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -10,8 +10,21 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25}/>
+      <AmbientLight intensity={0.25}/>
       <directionalLight position={[0, 0, 0.05]}/>
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+        color='#fff8eb'
+        polygonOffset
+        polygonOffsetFactor={-5}
+        flatShading
+        />
+        <Decal
+        position={[0, 0, 1]}
+        map={decal}
+        />
+      </mesh>
     </Float>
   )
 }
@@ -25,7 +38,7 @@ const BallCanvas = ({ icon }) => {
     >
       <Suspense fallback={<CanvasLoader/>}>
         <OrbitControls enableZoom={false}/>
-      <Ball imgUrl={isMobile}/>
+      <Ball imgUrl={icon}/>
       </Suspense>
       <Preload all/>
     </Canvas>
