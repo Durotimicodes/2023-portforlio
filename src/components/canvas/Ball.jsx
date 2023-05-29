fdb285507cdbc29e601d34946ad639529f39da3d
edmond fagbuyi <edmondfagbuyi@gmail.com>
2023-05-29 02:15:46 +0100

    adding 3d balls

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import {Decal, Float, OrbitControls, Preload, useTexture} from '@react-three/drei'
+import CanvasLoader from '../Loader';
+import { AmbientLight } from 'three';
 
-const Ball = () => {
+
+const Ball = (props) => {
+
+  const [decal] = useTexture([props.imgUrl]);
   return (
-    <div>Ball</div>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25}/>
+      <directionalLight position={[0, 0, 0.05]}/>
+    </Float>
   )
 }
 
-export default Ball
+const BallCanvas = ({ icon }) => {
+
+  return (
+    <Canvas
+      frameLoop="demand"
+    gl={{ preserveDrawingBuffer: true}}
+    >
+      <Suspense fallback={<CanvasLoader/>}>
+        <OrbitControls enableZoom={false}/>
+      <Ball imgUrl={isMobile}/>
+      </Suspense>
+      <Preload all/>
+    </Canvas>
+  )
+
+}
+
+export default BallCanvas
